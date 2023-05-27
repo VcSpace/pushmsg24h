6520876b7329523718589ec00486dc8c006667dd
Vc <vcvckw@gmail.com>
2023-05-27 16:38:31 +0800

    add

--- src/tokeninsight.py
+++ src/tokeninsight.py
@@ -4,57 +4,39 @@ import json
 import distutils
 
 from requests.cookies import RequestsCookieJar
+from src.config import config
 from src.push_msg import Pmsg
 
 class TOKEN_IN(object):
     def __init__(self):
         self.topid = '0'
         self.date = ''
+        self.ti_api_key = config.get_ti_key()
 
     def get_news(self):
         while True:
-            session = requests.Session()
             # cookie_jar = session.get('https://cn.tokeninsight.com/zh/news').cookies
             # cookie_t = requests.utils.dict_from_cookiejar(cookie_jar)
             m_json = ''
+            # url = "https://api.tokeninsight.com/api/v1/news/list"
             url = 'https://tokeninsight.com/apiv2/research/newsList'
-            url = 'https://back.tokeninsight.com/app/news/list?date=&page=1&size=30'
-            headers2 = {
-                'Accept': 'application/json, text/plain, */*',
-                'referer': 'https://tokeninsight.com/zh/news',
-                'Content-Type':'application/json',
-                'Cookie': 'language_=zh; language=cn; cf_bm=18BF4wWkNwA9vouobb_pe3ZNfmODPDeekfZIVIP88Is-1684942722-0-AVpxJJgapGxbRLA0SOFl0v6XV3b7AQA/6/GqM3My8AIUaVe4pINfeyTais+qRRbr3XJzVY1jPJv0DeVchdvzKT0=; SERVERID=efe99b30cbeeac07a0a49ddc4c447aee|1684943591|1684942722; SERVERCORSID=efe99b30cbeeac07a0a49ddc4c447aee|1684943591|1684942722',
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Safari/605.1.15'
-            }
 
             headers = {
-                'Host': 'back.tokeninsight.com',
-                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-                'Accept': '*/*',
-                'lan': 'cn',
-                'platform': 'iOS',
-                'User-Agent': 'TokenInsight_iOS/1.3.4 (iPhone; iOS 16.1.1; Scale/3.00)',
-                'Connection': 'keep-alive'
-            }
-            data = '{{\"current\":1,\"language\":\"cn\",\"pageSize\":10,\"tagId\":\"\",\"startDate\":\"\",\"endDate\":\"\"}}'
-            data = {
-                "current":1,
-                "language":"cn",
-                "pageSize":10,
-                "tagId":"",
-                "startDate":"",
-                "endDate":""
+                "accept": "application/json",
+                "TI_API_KEY": "{0}".format(self.ti_api_key)
             }
 
+            data = {"current": 1, "language": "cn", "pageSize": 10, "tagId": "", "startDate": "", "endDate": ""}
+
             try:
-                res = session.get(url, headers=headers, timeout=60)
+                res = requests.post(url, headers=headers, data=data, timeout=65)
                 if res.status_code == 200:
                     one_json = json.loads(res.text)
                     m_json = one_json['data']['list']
                     print(m_json)
-                    if self.topid == m_json[0]['tagId']:
+                    if self.topid == m_json[0]['id']:
                         print('topid == json_topid' )
-                        time.sleep(300)
+                        time.sleep(6000)
                         continue
 
                     for n in range(len(m_json)):
@@ -67,12 +49,14 @@ class TOKEN_IN(object):
                             print("topid == json_topid")
                             break
                         else:
-                            Pmsg.sendmeg(news_url, content, content_title)
+                            Pmsg.sendmeg(news_url, content, content_title, 'ti')
                             time.sleep(3)
 
-                self.topid = m_json[0]['tagId']
-                time.sleep(300)
+                    self.topid = m_json[0]['tagId']
+                    print("发送成功 timesleep")
+                time.sleep(6000)
             except Exception as e:
+                time.sleep(6000)
                 print(e)
 
-tkin = TOKEN_IN()
+ti = TOKEN_IN()
