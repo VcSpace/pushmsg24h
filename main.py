bfd2417c8e16800573f7923e9376dd050786272a
Vc <vcvckw@gmail.com>
2023-07-20 22:54:00 +0800

    init

--- main.py
+++ main.py
@@ -3,23 +3,26 @@ import threading
 from src.jinse import Js
 from src.odaily import oda
 from src.babtc import babtc
-from src.tokeninsight import ti
-from src.sql_insert import Sql
 from src.tuoluo import Tl
+from src.CoinTime import coin_time
 
 def run():
     t1 = threading.Thread(target=Js.get_news, args=())
     t2 = threading.Thread(target=oda.get_news, args=())
     t3 = threading.Thread(target=Tl.get_news, args=())
+    t4 = threading.Thread(target=coin_time.get_news, args=())
     t1.start()
     t2.start()
     t3.start()
+    t4.start()
     t1.join()
     t2.join()
     t3.join()
+    t4.join()
 if __name__ == '__main__':
     run()
     # Js.get_news()
     # oda.get_news()
     # ti.get_news()
     # Tl.get_news()
+    # coin_time.get_news()
