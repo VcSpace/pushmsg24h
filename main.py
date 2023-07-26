7fe9cc0871f66f34d7dbfa2c71f51008b5984375
Vc <vcvckw@gmail.com>
2023-07-26 18:16:27 +0800

    init

--- main.py
+++ main.py
@@ -7,22 +7,20 @@ from src.tuoluo import Tl
 from src.CoinTime import coin_time
 
 def run():
-    t1 = threading.Thread(target=Js.get_news, args=())
-    t2 = threading.Thread(target=oda.get_news, args=())
-    t3 = threading.Thread(target=Tl.get_news, args=())
-    t4 = threading.Thread(target=coin_time.get_news, args=())
+    t1 = threading.Thread(target=Js.get_news, args=(), daemon=True)
+    t2 = threading.Thread(target=oda.get_news, args=(), daemon=True)
+    t3 = threading.Thread(target=Tl.get_news, args=(), daemon=True)
+    t4 = threading.Thread(target=coin_time.get_news, args=(), daemon=True)
     t1.start()
     t2.start()
     t3.start()
     t4.start()
-    t1.join()
-    t2.join()
-    t3.join()
-    t4.join()
+
+
 if __name__ == '__main__':
-    run()
+    # run()
     # Js.get_news()
     # oda.get_news()
     # ti.get_news()
     # Tl.get_news()
-    # coin_time.get_news()
+    coin_time.get_news()
